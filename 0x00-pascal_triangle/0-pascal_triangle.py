6228dbe18ac31e57d3ee43c894434ab21ca264c0
shaimaa <shaimafikry@yahoo.com>
2024-08-03 19:11:16 +0300

    add a new dir

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -9,19 +9,28 @@ def pascal_triangle(n):
     #plan
     # loop through every row
     # start row = 1
-    start_row = 1
-    n = n - 2
-    for row in range(n):
+    # row = length
+    # start_row = 1
+    # n = n - 2
+    for row in range(2, n):
         # initiizing new row
         row_list = []
         # list_length = prev row + 1
-        list_length = start_row + 1
-        for i in range(list_length):
-            target_row = start_row - row
-            idx = 0
-            element = all_list[target_row][idx] - 1 + all_list[target_row][idx]
-            row_list.append(element)
-            idx += 1
+        # list_length = start_row + 1
+        # loop through every row
+        # length decided by the row umber
+        # idx is the start point of new added elements
+        idx = row - 1
+        start_row = row
+        for i in range(start_row):
+            target_row = start_row - 1
+            # add the old nums
+            for m in range(start_row):
+              row_list.append(all_list[target_row][m])
+              # how to make it only limitid to the new elemnts
+              if m >= idx and m < start_row:
+                # i want to add oly the new num
+                  row_list[m] = all_list[target_row][m - 1] + all_list[target_row][m]
         start_row += 1
         all_list.append(row_list)
     return all_list
