b9e7bfb361409edfd73d2434092f439828c7c8df
shaimaa <shaimafikry@yahoo.com>
2024-08-04 19:31:00 +0300

    add a new task

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+    module for the functon (canUnlockall)
+    """
+def canUnlockAll(boxes):
+    """_summary_
+        method that determines
+        if all the boxes can be opened.
+    Args:
+        boxes (list): list of lists
+    Return:
+        boolen: 
+    """
