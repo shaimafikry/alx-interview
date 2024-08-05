7f15a263a7de36cea9d238bc416228bc6f86df02
Shimaa Fikry <41205343+shaimafikry@users.noreply.github.com>
2024-08-05 04:56:10 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,10 +11,18 @@ def canUnlockAll(boxes):
     Return:
         boolen: 
     """
+   # edge cases
+   if len(boxes) == 1:
+      return True
+   # get the list length
 	  n = len(boxes)
+   # have first keys as a start
+   # make a deep copy of the orginal list
 	  keys = boxes[0][:]
 	  for i in range (1, n):
+     # check if i in the list of keys
 	    if i in keys:
+       # add a new keys to the list
 	      keys += boxes[i]
 	    else:
 	      return False	      
