0d4747ba41df0ca25cf751f76417adb8ad99d4c0
shaimaa <shaimafikry@yahoo.com>
2024-08-06 12:19:16 +0300

    task solved

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -14,21 +14,27 @@ def canUnlockAll(boxes):
         boolen: true if all opened
                 false if not
     """
-    # edge cases
-    if len(boxes) == 1:
-        return True
     # get the list length
     n = len(boxes)
+    # edge cases
+    if n == 1:
+        return True
     # have first keys as a start
     # make a deep copy of the orginal list
     keys = boxes[0][:]
-    # get the keys based on index
-    for i in range (1, n):
-        # check for i in existing keys
-        if i in keys:
-            keys += boxes[i]
-    for i in range(1, n):
-        # check if i in the list of keys
-        if i not in keys:
-            return False
-    return True
+    # to hold opened boxes
+    opened = set([0])
+    for i in keys:
+        # check if the key is in range
+        if i < n:
+            # if the box opened
+            opened.add(i)
+            for x in boxes[i]:
+                # add only unique values
+                # to prevent infinity loop
+                if x not in keys:
+                    keys.append(x)
+    # check if all boxes opened
+    if len(opened) == n:
+        return True
+    return False
--- 0x01-lockboxes/README.md
+++ 0x01-lockboxes/README.md
@@ -14,15 +14,20 @@ Return True if all boxes can be opened, else return False
 
 # My attemps
 
+- get the list length as n
 - check for edge case => list of 1 length
-- get the list length
 - get the first keys as a start
-- loop from index 1 till the end
-- check if i have the key for the next list
-- add the new keys to the keys list if true
-- if false, return false end of program
+- define a set to hold the opend boxes (unique values)
+- loop through the keys
+- check if i in keys within the range of n
+- add the i to the opened boxes
+- loop through the values of boxes[i] to add a new keys
+- make sure that the key isnt repeated in list
+- check the len of opend boxes and return the value
 
 # problems faced
 
-الحمدلله, that one was easy.
-
+الحمدلله, that one was easy. (said that too early >_<)
+- tried to choose something tht can hold a unique values from the start
+instead od if condtions ( alot of) so i used set for the opened boxes
+- faced a problem to do the same for keys, an error called ( set size changed during itrabling) so i had to make it a list with an if condtion to make the value unique
