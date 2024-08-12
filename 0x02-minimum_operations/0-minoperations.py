9b863181931ede983608845220e4eb3fb1a8e909
shaimaa <shaimafikry@yahoo.com>
2024-08-12 10:07:47 +0300

    add readme and task0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -28,27 +28,30 @@ def minOperations(n):
         return 2
     # define varibales
     # i have an H as default
-    count_h = 1
+    h = 1
     # there is always a copy_all and paste at least once
-    count_oper = 0
-    half_n = n / 2
-    copied_h = 1
+    operation = 0
+    third_n = n / 3
+    copy = 1
     goal = 0
-    while count_h < n:
+    while h < n:
         # check if it less tha a half
-        if count_h < half_n:
+        if h < third_n:
             # copy and paste the value
-            count_oper += 2
-        elif count_h == half_n:
+            operation += 2
+            copy = h
+        elif h == third_n:
             # copy , paste
-            count_oper += 2
-        elif count_h > half_n:
-            goal = count_h - half_n
-            if goal < copied_h:
+            operation += 2
+            copy = h
+        elif h > third_n:
+            goal = h - third_n
+            if goal <= copy:
                 # only paste
-                count_oper += 1
-        # print(f" before copied_h {copied_h} count_h {count_h}")
-        count_h += copied_h
-        copied_h = count_h
-        # print(f"copied_h {copied_h} count_h {count_h}")
-    return count_oper
+                if (h + copy) >= n:
+                    operation += 1
+                else:
+                    operation += 2
+                    copy = h
+        h += copy
+    return operation
