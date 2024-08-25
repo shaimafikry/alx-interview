07c194db5c6d8766c19d1ea90067fc81e5f9bff7
shaimaa <shaimafikry@yahoo.com>
2024-08-25 20:13:19 +0300

    add #2

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Main file for func
+"""
+
+
+def validUTF8(data):
+    """method that determines if a given data set
+      represents a valid UTF-8 encoding.
+    Return: True if data is a valid UTF-8 encoding,
+            else False
+"""
+    for i in data:
+        temp = bin(i)
+        # del the 0b at first
+        temp = temp[2:]
+        # print(len(temp))
+        # to check if its one byte or two (more than 8 bits)
+        if temp[0] == '1':
+            if len(temp) < 9:
+                continue
+            else:
+                if len(temp) == 9:
+                    # to check for 10xxxxxx 10xxxxxx
+                    if temp[0] == '1' and temp[8] == '1':
+                        continue
+                    else:
+                        return False
+    return True
