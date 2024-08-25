c7a67ff2c7acba88d0fd7d9647f6f40122eebf11
shaimaa <shaimafikry@yahoo.com>
2024-08-25 20:25:54 +0300

    add #2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -17,16 +17,18 @@ def validUTF8(data):
         # print(len(temp))
         # to check if its one byte or two (more than 8 bits)
         if temp[0] == '1':
-            if len(temp) < 9:
+            b_len = len(temp)
+            if b_len < 9:
                 continue
             else:
-                if len(temp) == 9:
+                if b_len >= 9:
                     # to check for 10xxxxxx 10xxxxxx
                     if temp[8] == '1':
-                        if temp[9] == '1' or temp[9] == 0:
-                            continue
-                        else:
-                            return False
+                        if b_len > 9:
+                            if temp[9] == '1' or temp[9] == 0:
+                                continue
+                            else:
+                                return False
                     else:
                         return False
     return True
