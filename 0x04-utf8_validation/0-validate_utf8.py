925c8c40e6bf22caa9386a961314670bd45ce7dc
shaimaa <shaimafikry@yahoo.com>
2024-08-25 20:21:25 +0300

    add #2

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -23,8 +23,10 @@ def validUTF8(data):
                 if len(temp) == 9:
                     # to check for 10xxxxxx 10xxxxxx
                     if temp[8] == '1':
-                      if temp[9] == '1' or temp[9] == 0:
-                        continue
+                        if temp[9] == '1' or temp[9] == 0:
+                            continue
+                        else:
+                            return False
                     else:
                         return False
     return True
