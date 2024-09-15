0852567fb66c6fb7cb355425bc50c0d7f46b4fd5
shaimaa <shaimafikry@yahoo.com>
2024-09-16 00:02:59 +0300

    add new task

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,6 +3,15 @@
 Test 0x07 - Rotate 2D Matrix
 """
 
+
 def rotate_2d_matrix(matrix):
     """mtrix rotate func"""
-    
+    # make the verticl horzintal
+    length = len(matrix)
+    for r in range(length):
+        # to only conver the column at a loop not all the row
+        for c in range(r, length):
+            matrix[r][c], matrix[c][r] = matrix[c][r], matrix[r][c]
+
+    for i in matrix:
+        i.reverse()
