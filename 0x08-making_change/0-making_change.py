ca0ee61d135a42ae6c31377bd15c74172cd73ff0
shaimaa <shaimafikry@yahoo.com>
2024-09-22 14:37:57 +0300

    add 6

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -16,7 +16,7 @@ def makeChange(coins, total):
     coins.sort(reverse=True)
 
     # case total is less than the ssmallest coin
-    if total < coins[-1]:
+    if total < coins[-1] or len(coins) == 0:
         return -1
 
     for coin in coins:
